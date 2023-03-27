e6161816bc35aa647bf0e10a4c46ca45236892b9
MarkRywell <gopro855@gmail.com>
2023-03-27 20:19:36 +0800

    test: :art: Add Seeders for testing

--- database/seeders/EmployeeSeeder.php
+++ database/seeders/EmployeeSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Employee;
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class EmployeeSeeder extends Seeder
 {
@@ -12,6 +15,14 @@ class EmployeeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // $employees = [
+            // [
+                // 'name' => 'Mark Gaje',
+                // 'email' => 'markgaje@gmail.com',
+                // 'password' => Hash
+            // ]
+        // ];
+
+        Employee::factory(1)->create(["email" => 'markgaje@gmail.com', "password" => Hash::make('mark123'), "name" => 'Mark Gaje', 'role_id' => 1, "status" => true]);
     }
 }
