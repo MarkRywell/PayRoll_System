4b884ec5daf84d28c6929bbf727444f35306f671
MarkRywell <gopro855@gmail.com>
2023-03-27 20:21:12 +0800

    feat: :art: Add controller for Employees and Auth

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Employee;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        $responseData = [
+            'status' => 'fail',
+            'message' => '',
+            'data' => null
+        ];
+      
+        
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|string|unique:employees',
+            'password' => 'required|string|min:6',
+        ]);
+        
+        if ($validator->fails()) {
+            $responseData['message'] = $validator->errors()->first();
+            
+            return response()->json($responseData, 400);
+        }
+
+        
+
+        $employee = Employee::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+            'role' => 'Employee',
+            'status' => true
+        ]);
+
+        if ($employee == null)
+        {   
+            $responseData['message'] = 'Unsuccessful Registration';
+            return response()->json($responseData, 400);
+        }
+        
+        $responseData['status'] = 'success';
+        $responseData['message'] = 'Successful Registration';
+
+        return response()->json($responseData, 201);
+    }
+
+
+
+
+
+}
--- app/Http/Controllers/Api/EmployeeController.php
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -21,7 +21,7 @@ class EmployeeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
