af524a449557086298e9bf4d4aa3054b98f9d925
MarkRywell <gopro855@gmail.com>
2023-03-30 16:09:16 +0800

    refactor: :recycle: Update on model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id',
+        'status',
+        'photo'
     ];
 
     /**
@@ -38,7 +41,4 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
