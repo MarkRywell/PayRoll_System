22c5d6e053435e3a7607adcaefe4c6a88e17662b
MarkRywell <gopro855@gmail.com>
2023-03-30 16:42:40 +0800

    feat: :sparkles: Update and addPhoto in User (not yet tested)

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -21,7 +22,7 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
@@ -29,15 +30,67 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
-        //
+        
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, User $user)
+
+    public function addPhoto(Request $request, int $id)
     {
-        //
+        $responseData = [
+            'status' => 'fail',
+            'message' => '',
+            'data' => null
+        ];
+
+        $file_path = $request['photo']->store('public/uploads');
+        $file_name = str_replace("public/uploads/", "", $file_path);
+
+
+        $response = DB::table('users')
+        ->where('id', $id)
+        ->update([
+            'photo' => $file_name
+        ]);
+
+        if($response != null)
+        {
+            $responseData['status'] = 'success';
+            $responseData['message'] = 'Profile Photo Added';
+            $responseData['data'] = $file_name;
+            return response()->json($responseData, 200);
+        }
+        
+        $responseData['message'] = 'Photo Upload Unsuccessful';
+        return $responseData;
+    }
+
+    public function update(Request $request, int $id)
+    {
+        $responseData = [
+            'status' => 'fail',
+            'message' => '',
+            'data' => null
+        ];
+
+        $response = DB::table('users')
+        ->where('id', $id)
+        ->update([
+            'name' => $request['name'],
+            'photo' => $request['photo']
+        ]);
+
+        if($response != null)
+        {
+            $responseData['status'] = 'success';
+            $responseData['message'] = 'Update Successful';
+            return response()->json($responseData, 200);
+        }
+        
+        $responseData['message'] = 'Update Unsuccessful';
+        return $responseData;
     }
 
     /**
