dbbcd3ab9f95aab33d22be98cc20b8a8de5c7a92
MarkRywell <gopro855@gmail.com>
2023-04-05 13:20:00 +0800

    Add new controller for payroll

---
+++ app/Http/Controllers/Api/PayrollController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\PayRoll;
+use Illuminate\Http\Request;
+
+class PayrollController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PayRoll $payRoll)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PayRoll $payRoll)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PayRoll $payRoll)
+    {
+        //
+    }
+}
