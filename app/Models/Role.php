278850e724dcad15b21711b546db87e965abcbd7
MarkRywell <gopro855@gmail.com>
2023-04-07 19:08:29 +0800

    Refactor on Role Model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory;
 
     public function user() {
         return $this->hasMany(User::class);
