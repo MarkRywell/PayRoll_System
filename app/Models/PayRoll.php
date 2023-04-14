cb9ff6f6d59915d8e495053424cf56bc5a7a7bb4
MarkRywell <gopro855@gmail.com>
2023-04-14 15:52:57 +0800

    Add create payroll inside Payroll model

--- app/Models/PayRoll.php
+++ app/Models/PayRoll.php
@@ -21,4 +21,17 @@ class PayRoll extends Model
         'working_days',
         'salary'
     ];
+
+    public static function createPayRoll($request)
+    {
+        $payroll = PayRoll::create([
+            'rate' => $request['rate'],
+            'month' => $request['month'],
+            'working_days' => $request['working_days'],
+            'salary' => $request['salary'],
+            'user_id' => $request['user_id']
+        ]);
+
+        return $payroll;
+    }
 }
