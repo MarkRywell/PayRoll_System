caf0112ed480e3eb6494b59fdd1f040a5c3633e9
MarkRywell <gopro855@gmail.com>
2023-04-26 21:03:01 +0800

    feat: :sparkles: Add Salary Model with createSalary function

---
+++ app/Models/Salary.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Salary extends Model
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    public function payroll() {
+        return $this->belongsTo(PayRoll::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    protected $fillable = [
+        'payroll_id',
+        'user_id',
+        'gross_salary',
+        'deduction',
+        'net_salary',
+    ];
+
+    public static function createSalary($request)
+    {
+        $salary = Salary::create([
+            'payroll_id' => $request['payroll_id'],
+            'user_id' => $request['user_id'],
+            'gross_salary' => $request['gross_salary'],
+            'deduction' => $request['deduction'],
+            'net_salary' => $request['net_salary']
+        ]);
+
+        return $salary;
+    }
+
+    
+}
