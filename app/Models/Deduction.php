317030d1744674d9633a741b14878636482f9e9e
MarkRywell <gopro855@gmail.com>
2023-05-07 17:17:59 +0800

    feat: :sparkles: Update Deduction model
    Add create, fillables, and eloquent relationships

--- app/Models/Deduction.php
+++ app/Models/Deduction.php
@@ -4,8 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Deduction extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+    public function salary() {
+        return $this->belongsTo(Salary::class);
+    }
+
+    protected $fillable = [
+        'salary_id',
+        'sss',
+        'pagibig',
+        'philhealth',
+        'tax',
+        'total_deduction'
+    ];
+
+    public static function createDeduction($request)
+    {
+        $deduction = Deduction::create([
+            'salary_id' => $request['salary_id'],
+            'sss' => $request['sss'],
+            'pagibig' => $request['pagibig'],
+            'philhealth' => $request['philhealth'],
+            'tax' => $request['tax'],
+            'total_deduction' => $request['total_deduction'],
+        ]);
+    }
+
+
 }
