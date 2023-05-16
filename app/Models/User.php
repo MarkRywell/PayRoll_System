c149475bb88b68d536cf9f81da11c09c059016ae
MarkRywell <gopro855@gmail.com>
2023-05-16 08:17:20 +0800

    refactor: :ambulance: Fix issue on getUserbyEmail function

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,7 +44,7 @@ class User extends Authenticatable
 
     public static function getUserbyEmail($email)
     {
-        return User::get()->where('email', $email);
+        return User::where('email', $email)->first();
     }
 
     /**
