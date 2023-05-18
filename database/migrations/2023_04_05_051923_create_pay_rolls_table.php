d8d56ad19f4b1fd55f7ca191ac06fe54a1a24ec8
MarkRywell <gopro855@gmail.com>
2023-05-18 20:56:58 +0800

    Update migration on pay_rolls

--- database/migrations/2023_04_05_051923_create_pay_rolls_table.php
+++ database/migrations/2023_04_05_051923_create_pay_rolls_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('pay_rolls', function (Blueprint $table) {
             $table->id();
-            $table->double('rate', 6, 2);
             $table->string("month");
             $table->integer('working_days');
             $table->integer('total_hours_overtime');
