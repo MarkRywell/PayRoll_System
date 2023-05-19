7fdabdd7ac5b301f214815457f6cb87578c80fbc
MarkRywell <gopro855@gmail.com>
2023-05-19 16:04:21 +0800

    refactor: :art: Update users migration

--- database/migrations/2023_03_24_000000_create_users_table.php
+++ database/migrations/2023_03_24_000000_create_users_table.php
@@ -13,12 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('role_id')->constrained('roles')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('address_id')->constrained('addresses')->onDelete('cascade')->onUpdate('cascade');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('contact_number');
             $table->string('position');
             $table->double('rate', 6, 2);
-            $table->foreignId('role_id')->constrained('roles')->onDelete('cascade')->onUpdate('cascade');
             $table->boolean('status');
             $table->string('photo')->nullable();
             $table->timestamp('email_verified_at')->nullable();
